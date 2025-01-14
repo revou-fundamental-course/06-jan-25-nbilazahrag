eed093f9a63e9294671c0a179046265dd2e148ef
unknown <naghnia11@gmail.com>
2025-01-15 01:42:56 +0700

    Second Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nameInput = document.getElementById('name-input')
+
+document.getElementById('submit').addEventListener("click", function() {
+    const nama = nameInput.value
+
+    document.getElementById('name').innerHTML = nama
+})
