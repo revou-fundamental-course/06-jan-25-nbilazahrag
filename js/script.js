b7a95ad92cf08b77d2bcd8ff8262db7e95373387
unknown <naghnia11@gmail.com>
2025-01-15 19:59:01 +0700

    Fourth Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//slide
 let currentIndex = 0;
 const slides = document.querySelectorAll('.slider img');
 const totalSlides = slides.length;
@@ -32,10 +33,43 @@ navLinks.forEach((link, index) => {
 
 setInterval(nextSlide, 3000);
 
+//rubah nama
+
 showSlide(currentIndex);
 
 document.getElementById('submit').addEventListener("click", function(e) {
     e.preventDefault();
     const nameInput = document.getElementById('name-input').value;
     document.getElementById('name').textContent = nameInput;
+});
+
+//im a... autotype
+const autoTypeElement = document.querySelector('#autoType .auto-type');
+const words = ["Graphic Designer", "FrontEnd Developer", "Content Creator"];
+let wordIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < words[wordIndex].length) {
+        autoTypeElement.textContent += words[wordIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 150); 
+    } else {
+        setTimeout(erase, 2000); 
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        autoTypeElement.textContent = words[wordIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, 100); 
+    } else {
+        wordIndex = (wordIndex + 1) % words.length;
+        setTimeout(type, 500); 
+      }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    type();
 });
